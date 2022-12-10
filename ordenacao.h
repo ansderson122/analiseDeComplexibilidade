0ef0b381214da4b78d61053d178913a02a4d4359
Julyanderson Alves <ansderson84001@gmail.com>
2022-12-10 16:59:23 -0300

    first commit

---
+++ ordenacao.h
@@ -0,0 +1,12 @@
+#ifndef ORDENACAO_H_INCLUDED
+#define ORDENACAO_H_INCLUDED
+
+void bubblesort(int *, int);
+void selectionSort(int *, int);
+void insertionSort(int *, int);
+void mergeSort(int *, int, int);
+void quickSort(int *, int, int);
+int* criaVetor(int);
+void imprimeVetor(int*, int);
+int media(int[10],int);
+#endif // ORDENACAO_H_INCLUDED
---
+++ test-bubblesort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "ordenacao.c"
+#include "ordenacao.h"
+
+void *testBubblesort(int *i[2]){
+	clock_t tempo,aux; 
+    int* vetor = criaVetor(*i[0]);
+    tempo = clock();
+    bubblesort(vetor, *i[0]);
+    tempo = clock() - tempo;
+    *i[1] = tempo;
+    free(vetor);    
+}
+
+int main(void){
+	clock_t aux = 0;
+    printf("Teste para o bubblesort\n\n");
+    int i,j,*entrada[2],tempos[10];
+    for(i = 10; i <= 1000000; i=i*10){
+        printf("Vetor tamanho %d:\n",i);
+		entrada[0] = &i;
+		
+        for(j = 0; j < 10; j++){
+        	tempos[j] = 0;
+			entrada[1] = &tempos[j];
+        	testBubblesort(entrada);           
+        }
+		
+		
+		printf("Os tempos de execucao foram:");
+		imprimeVetor(tempos, 10); 		
+    	aux = media(tempos,10);
+        printf("\nTempo medio de execucao: %lf milissegundos\n\n", ((double)aux)/((CLOCKS_PER_SEC/1000)));
+        aux = 0;
+		
+		
+    }
+
+    return 1;
+}
